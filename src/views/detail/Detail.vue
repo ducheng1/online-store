cfb6772eefe390cd4ad3517410da88915f63ee57
崔栋凯 <2441919651@qq.com>
2022-05-30 18:08:18 +0800

    cdk

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,46 +1,73 @@
 <template>
-    <div id="container">
-        DetailPage
-        <div id="detail">
-            <div id="detail-left">
-                <div id="pic">
-                    <img src="./images/1.jpg"/>
-                </div>
-            </div>
-            <div id="detail-right">
-                <div id="detail-title">
-                    上班族社畜打工人猫咪设计猫可爱卡通纯棉短袖t恤衫男女半袖衣服
-                </div>
-                <div id="detail-price">
-                    65
-                </div>
-                <div id="buy">
-                    <button>购买</button>
-                    <button @click="gowuche">加入购物车</button>
-                </div>
-            </div>
+  <div id="container">
+    <div id="detail">
+      <div id="detail-left">
+        <div id="pic">
+          <img v-bind:src="good.url" />
         </div>
+      </div>
+      <div id="detail-right">
+        <div id="detail-title">
+          {{ good.title }}
+        </div>
+        <div id="detail-price">{{ good.price }}</div>
+      </div>
     </div>
+    <div id="foot">
+        <van-action-bar>
+          <van-action-bar-icon icon="chat-o" text="客服" color="#ee0a24" />
+          <van-action-bar-icon icon="cart-o" text="购物车" />
+          <van-action-bar-icon icon="star" text="未收藏"  />
+          <van-action-bar-icon icon="star" id="ab1" text="已收藏" color="#ff5000" />
+          <van-action-bar-button type="warning" text="加入购物车" @click="gowuche" />
+          <van-action-bar-button type="danger" text="立即购买" />
+        </van-action-bar>
+    </div>
+  </div>
 </template>
 <script>
-import {useRouter} from 'vue-router'
-import {useStore} from 'vuex'
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
+
 export default {
-    name: "DetailPage",
-    setup(){
-        const router=useRouter();
-        let id=router.currentRoute.value.query.id;
-        console.log(id);
-        const store=useStore();
-        let goods=store.state.goods;
-        console.log(goods);
-        function gowuche(){
-            store.commit("add",id);
-        }
-        return {gowuche}
+  name: "DetailPage",
+
+  setup() {
+    const router = useRouter();
+    let id = router.currentRoute.value.query.id;
+    console.log(id);
+    const store = useStore();
+    let goods = store.state.goods;
+    let good;
+    goods.forEach((element) => {
+      console.log(element);
+      if (element.id == id) good = element;
+    });
+    console.log(good);
+    function gowuche() {
+      store.commit("add", id);
     }
 
+    const themeVars = {
+      
+    };
+    return {
+      gowuche,
+      good,
+      themeVars,
+    };
+  },
 };
 </script>
-<style scoped>
+<style scope>
+img {
+  width: 100%;
+  height: auto;
+}
+.van-action-bar {
+bottom: 50px;
+}
+#ab1{
+    display: none;
+}
 </style>
